85871c634346aaa460e257ead7d12cba9d00a98a
izzie1A <47637788+izzie1A@users.noreply.github.com>
2023-10-28 05:54:47 +0100

    ngWebApp2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ItemCardComponent } from './components/item-card/item-card.component';
+import { ItemCardDetailComponent } from './components/item-card-detail/item-card-detail.component';
+import { ItemCardDialogComponent } from './components/item-card-dialog/item-card-dialog.component';
+import { ItemCardListComponent } from './components/item-card-list/item-card-list.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemCardComponent,
+    ItemCardDetailComponent,
+    ItemCardDialogComponent,
+    ItemCardListComponent,
+    NavBarComponent,
+    SignInComponent
   ],
   imports: [
     BrowserModule,
