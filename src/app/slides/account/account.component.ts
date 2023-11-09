c9a08eaccc1e0f9546f02600aa1964489252e1e3
izzie1A <47637788+izzie1A@users.noreply.github.com>
2023-11-09 09:03:25 +0000

    second update

--- src/app/slides/account/account.component.ts
+++ src/app/slides/account/account.component.ts
@@ -9,6 +9,7 @@ import { AuthService } from "src/app/services/auth.service";
 })
 export class AccountComponent {
   templateUser: any;
+  registerLog:string|undefined;
 
   constructor(public authS: AuthService) {
     const user = this.authS.authState$;
@@ -26,8 +27,8 @@ export class AccountComponent {
   async emailRegister(email: string, password1: string, password2: string) {
     let x = this.isRegisterValidate({ email: email });
     if (x != null && password1 == password2) {
-      let e = await this.authS.emailRegister(email,password1);
-      console.log(e)
+      this.registerLog = await this.authS.emailRegister(email,password1);
+      console.log(this.registerLog)
     }
   }
   emailSignin(email: string, password: string){
